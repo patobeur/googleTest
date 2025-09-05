643bcd2facdbc3ead243c9c91a80ef3fd449b789
Patobeur <patobeur41@gmail.com>
2025-09-05 16:42:32 +0200

    modified:   server/game.js

--- server/game.js
+++ server/game.js
@@ -1,77 +1,87 @@
-const jwt = require('jsonwebtoken');
-const { getPlayerByUserId, createPlayer, updatePlayer } = require('./db');
-const jwtSecret = 'supersecretkey'; // In a real app, use an environment variable
+const jwt = require("jsonwebtoken");
+const { getPlayerByUserId, createPlayer, updatePlayer } = require("./db");
+const jwtSecret = "supersecretkey"; // In a real app, use an environment variable
 
 function initGame(io) {
-    const players = {};
+	const players = {};
 
-    // Middleware d'authentification Socket.IO
-    io.use((socket, next) => {
-        const token = socket.handshake.auth.token;
-        if (!token) {
-            return next(new Error('Authentication error'));
-        }
-        jwt.verify(token, jwtSecret, (err, decoded) => {
-            if (err) {
-                return next(new Error('Authentication error'));
-            }
-            socket.userId = decoded.userId;
-            next();
-        });
-    });
+	// Middleware d'authentification Socket.IO
+	io.use((socket, next) => {
+		const token = socket.handshake.auth.token;
+		if (!token) {
+			return next(new Error("Authentication error"));
+		}
+		jwt.verify(token, jwtSecret, (err, decoded) => {
+			if (err) {
+				return next(new Error("Authentication error"));
+			}
+			socket.userId = decoded.userId;
+			next();
+		});
+	});
 
-    io.on('connection', (socket) => {
-        console.log('Un nouveau joueur est connecté:', socket.id, 'avec le userId:', socket.userId);
+	io.on("connection", (socket) => {
+		console.log(
+			"Un nouveau joueur est connecté:",
+			socket.id,
+			"avec le userId:",
+			socket.userId
+		);
 
-        let player = getPlayerByUserId(socket.userId);
-        if (!player) {
-            player = {
-                id: socket.id,
-                userId: socket.userId,
-                x: Math.floor(Math.random() * 400) + 50,
-                y: Math.floor(Math.random() * 400) + 50,
-                color: `hsl(${Math.random() * 360}, 100%, 50%)`
-            };
-            createPlayer(player);
-        } else {
-            player.id = socket.id; // Update socket id
-        }
+		let player = getPlayerByUserId(socket.userId);
+		if (!player) {
+			player = {
+				id: socket.id,
+				userId: socket.userId,
+				x: Math.floor(Math.random() * 400) + 50,
+				y: Math.floor(Math.random() * 400) + 50,
+				color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+			};
+			createPlayer(player);
+		} else {
+			player.id = socket.id; // Update socket id
+		}
 
-        players[socket.id] = player;
+		players[socket.id] = player;
 
-        socket.emit('currentState', players);
-        socket.broadcast.emit('newPlayer', player);
+		socket.emit("currentState", players);
+		socket.broadcast.emit("newPlayer", player);
 
-        socket.on('disconnect', () => {
-            console.log('Un joueur s\'est déconnecté:', socket.id);
-            updatePlayer(players[socket.id]);
-            delete players[socket.id];
-            io.emit('playerDisconnected', socket.id);
-        });
+		socket.on("disconnect", () => {
+			console.log("Un joueur s'est déconnecté:", socket.id);
+			updatePlayer(players[socket.id]);
+			delete players[socket.id];
+			io.emit("playerDisconnected", socket.id);
+		});
 
-        socket.on('playerMovement', (movementData) => {
-            const player = players[socket.id];
-            if (!player) return;
+		socket.on("playerMovement", (movementData) => {
+			const player = players[socket.id];
+			if (!player) return;
 
-            const lastPosition = { x: player.x, y: player.y };
-            const newPosition = movementData;
+			const lastPosition = { x: player.x, y: player.y };
+			const newPosition = movementData;
 
-            const distanceSq = Math.pow(newPosition.x - lastPosition.x, 2) + Math.pow(newPosition.y - lastPosition.y, 2);
+			const distanceSq =
+				Math.pow(newPosition.x - lastPosition.x, 2) +
+				Math.pow(newPosition.y - lastPosition.y, 2);
 
-            const MAX_SPEED = 5;
-            const TOLERANCE_FACTOR = 1.5;
-            const MAX_DISTANCE_SQ = Math.pow(MAX_SPEED * TOLERANCE_FACTOR, 2);
+			const MAX_SPEED = 5;
+			const TOLERANCE_FACTOR = 1.5;
+			const MAX_DISTANCE_SQ = Math.pow(MAX_SPEED * TOLERANCE_FACTOR, 2);
 
-            if (distanceSq <= MAX_DISTANCE_SQ) {
-                player.x = newPosition.x;
-                player.y = newPosition.y;
-                socket.broadcast.emit('playerMoved', player);
-            } else {
-                console.log(`Mouvement invalide détecté pour le joueur ${socket.id}.`);
-                socket.emit('correction', lastPosition);
-            }
-        });
-    });
+			if (distanceSq <= MAX_DISTANCE_SQ) {
+				player.x = newPosition.x;
+				player.y = newPosition.y;
+				// socket.broadcast.emit('playerMoved', player);
+				io.emit("playerMoved", player);
+			} else {
+				console.log(
+					`Mouvement invalide détecté pour le joueur ${socket.id}.`
+				);
+				socket.emit("correction", lastPosition);
+			}
+		});
+	});
 }
 
 module.exports = { initGame };
